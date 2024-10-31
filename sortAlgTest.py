31e597d3b8c046779add201daf4ae929c5cf0d29
cdbelala <149855573+cdbelala@users.noreply.github.com>
2024-10-31 13:19:45 -0500

    Update sortAlgTest.py

--- sortAlgTest.py
+++ sortAlgTest.py
@@ -2,11 +2,19 @@ import unittest
 from sortAlg import quicksort
 
 class quicksortTest(unittest.TestCase):
-    def testEmptyList(self):
+    def testEmpty(self):
         self.assertEqual(quicksort([]), [])
-    def testOneElement(self):
+    def testOneElem(self):
         self.assertEqual(quicksort([1]), [1])
     def testSorted(self):
         self.assertEqual([1, 2, 3, 4, 5]), ([1, 2, 3, 4, 5])
-    def test_with_duplicates(self):
+    def testDupes(self):
         self.assertEqual(quicksort([3, 3, 3, 3]), [3, 3, 3, 3])
+    def testManyRuns(self):
+        arr = [3, 6, 8, 10, 1, 2, 1]
+        sortOnce = quicksort(arr)
+        sortTwice = quicksort(sortOnce)
+        self.assertEqual(sortOnce, sortTwice)
+    def testIsString(self):
+        with self.assertRaises(TypeError):
+            quicksort([9, "o", 3])
